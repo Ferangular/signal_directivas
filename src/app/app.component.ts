ef1c5c55070041b84e3ef2d81b269e8ca1159b2c
Ferangular <firthunsnpm@gmail.com>
2024-05-24 22:53:24 +0200

    Inicio del proyecto angular 18, configuración inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -6,8 +6,22 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {
   title = 'signal_directivas';
+  parent(){
+    console.log('parent');
+  }
+
+  child(){
+    console.log('child')
+  }
+
+  protected name = signal('Angular');
+
+  handleClick() {
+    this.name.set('Zoneless Angular');
+  }
 }
